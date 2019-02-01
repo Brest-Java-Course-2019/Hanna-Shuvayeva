6186e9e440285edfdf5ce026ecd1d96a29c608da
Hanna-Shuvayeva <Hanna-Shuvayeva@example.com>
2019-02-01 15:11:45 +0200

    add tests for another class

---
+++ src/test/java/com/epam/brest/cources/calc/CalcTest.java
@@ -0,0 +1,18 @@
+package com.epam.brest.cources.calc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+class CalcTest {
+    @Test
+    void getShipCost_1km(){
+        Assert.assertTrue("null",true);
+        Assert.assertFalse("tyr",false);
+    }
+    @Test
+    void getShipCost_weight(){
+        Assert.assertTrue("null",true);
+        Assert.assertFalse("tyr",false);
+    }
+
+}
