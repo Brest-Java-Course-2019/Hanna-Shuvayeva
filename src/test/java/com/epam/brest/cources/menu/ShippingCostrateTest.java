3cc5f1e7b062c9eae0c9c269c2539782be076378
Hanna-Shuvayeva <Hanna-Shuvayeva@example.com>
2019-02-08 16:35:04 +0200

    fix tests

--- src/test/java/com/epam/brest/cources/menu/ShippingCostrateTest.java
+++ src/test/java/com/epam/brest/cources/menu/ShippingCostrateTest.java
@@ -1,19 +1,35 @@
 package com.epam.brest.cources.menu;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.Assert;
 
+import java.math.BigDecimal;
+
 class ShippingCostrateTest {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("@BeforeEach");
+    }
     @Test
     void getRate1(){
-        Assert.assertTrue("null",true);
-        Assert.assertFalse("tyr",false);
+        ValueImpl ob=new ValueImpl();
+        BigDecimal a = new BigDecimal(1.10);
+        BigDecimal actual= a.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+        Assert.assertEquals(ob.getRate1(), actual);
     }
     @Test
     void getRate2(){
-        Assert.assertTrue("null",true);
-        Assert.assertFalse("tyr",false);
+        ValueImpl ob=new ValueImpl();
+        BigDecimal a = new BigDecimal(1.20);
+        BigDecimal actual= a.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+        Assert.assertEquals(ob.getRate2(), actual);
     }
 
+    @AfterAll
+    static void afterAll() {
+        System.out.println("@AfterAll");
+    }
 
 }
