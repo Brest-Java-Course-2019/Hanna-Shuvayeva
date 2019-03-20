c8363f5a809f8821d4d219f557e17acc2ecec390
Hanna-Shuvayeva <Hanna-Shuvayeva@example.com>
2019-03-20 11:15:12 +0200

    bag file

--- FlightsCrews/web-app/src/main/java/com/epam/courses/hr/web_app/HomeController.java
+++ FlightsCrews/web-app/src/main/java/com/epam/courses/hr/web_app/HomeController.java
@@ -1,18 +1,14 @@
 package com.epam.courses.hr.web_app;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
 
-    /**
-     * Redirect to default page -> departments.
-     *     * @return redirect path
-     */
-   // @GetMapping(value = "/")
+    @GetMapping(value = "/")
     public String defaultPageRedirect() {
-        return "redirect:departments";
+        return "redirect:flights";
     }
 }
